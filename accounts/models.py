c5fb9eedea81cc04e7116c6fc865d08db2d08fda
Arthur Oleinikov <arch0998@gmail.com>
2025-08-16 13:05:37 +0300

    Add Follow model to accounts/models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
 
 
 class User(AbstractUser):
@@ -26,3 +27,22 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return f"Profile: {self.user.username}"
+
+
+class Follow(models.Model):
+    follower = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="following_set"
+    )
+    following = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="followers_set"
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    class Meta:
+        unique_together = ("follower", "following")
