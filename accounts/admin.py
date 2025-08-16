aaff853999953b14dd727bad7f2dd3427a1cfb68
Arthur Oleinikov <arch0998@gmail.com>
2025-08-16 14:51:43 +0300

    Register User and Follow models in admin with custom display and search fields

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from django.contrib.auth.admin import Group, UserAdmin
 
-# Register your models here.
+from accounts.models import User, UserProfile, Follow
+
+
+admin.site.unregister(Group)
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = [
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "phone",
+        "is_active",
+        "is_staff",
+    ]
+    search_fields = [
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "phone",
+    ]
+
+
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    list_display = [
+        "follower",
+        "following"
+    ]
+    search_fields = [
+        "follower__username",
+        "following__username"
+    ]
+    list_filter = [
+        "follower",
+        "following"
+    ]
