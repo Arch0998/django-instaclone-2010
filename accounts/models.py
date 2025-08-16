f23708e91859ecd340cd614d5fec4a3a63f8203b
Arthur Oleinikov <arch0998@gmail.com>
2025-08-16 12:29:36 +0300

    Add custom User model and UserProfile

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    phone = models.CharField(
+        max_length=18,
+        unique=True
+    )
+
+    def __str__(self):
+        return self.username
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name="profile"
+    )
+    avatar = models.ImageField(
+        upload_to="avatars/",
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return f"Profile: {self.user.username}"
