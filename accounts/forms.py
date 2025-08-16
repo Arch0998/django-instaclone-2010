db86f2d72d1a0ac88b8d600a18ac84d8eb56e851
Arthur Oleinikov <arch0998@gmail.com>
2025-08-16 15:16:12 +0300

    Add UserRegisterForm for user registration with custom fields

---
+++ accounts/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
+from accounts.models import User
+
+
+class UserRegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = [
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "phone",
+            "password1",
+            "password2"
+        ]
